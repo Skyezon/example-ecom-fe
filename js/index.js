2dd2dafb2adf7f99680675fe33fed5e9f67e48ca
Delbert <Skyezon@users.noreply.github.com>
2019-12-11 11:49:31 +0700

    fixing index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}/`);
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
